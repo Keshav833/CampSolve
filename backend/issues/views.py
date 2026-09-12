741342fc15e5f952a419973535db8b658289a1fc
Keshav Chauhan <korplz1408@gmail.com>
2026-09-12 16:39:27 +0530

    added edit issue feature

--- backend/issues/views.py
+++ backend/issues/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from django.http import HttpResponse
 from .models import Issue
 from .forms import IssueForm
+
 
 def issue_list(request):
 
@@ -31,4 +32,31 @@ def create_issue( request):
         {"form":form}
     )
 
+def issue_detail(request, issue_id):
+    issue = get_object_or_404(Issue, id = issue_id)
 
+    return render(
+        request , 
+        "issues/issue_detail.html",
+        {"issue":issue}
+    )
+
+def edit_issue(request, issue_id):
+    issue = get_object_or_404(Issue, id = issue_id)
+
+    if request.method == "POST":
+        form = IssueForm(request.POST,instance=issue)
+
+        if form.is_valid():
+            form.save()
+            return redirect("issue-detail",issue_id = issue.id)
+
+    else:
+        form = IssueForm(instance=issue)
+
+    return render(
+        request,
+        "issues/edit_issue.html",
+        {"form":form ,"issue":issue}
+    )
+    
