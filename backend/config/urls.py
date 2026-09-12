6156d5a3d98faf46397d956ef8e842780f6172d8
Keshav Chauhan <korplz1408@gmail.com>
2026-09-12 17:21:33 +0530

    created accounts app

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,5 +20,6 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("issues/", include("issues.urls")),
+    path("accounts/", include("accounts.urls")),
 
 ]
