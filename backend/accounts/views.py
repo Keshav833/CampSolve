9847d0cbdfad6e7204784cad8867f357a060e3aa
Keshav Chauhan <korplz1408@gmail.com>
2026-09-12 18:22:08 +0530

    feat: add user registration and login

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return redirect("login")
+    else:
+         form = RegisterForm()
+
+    return render(
+        request,
+        "accounts/register.html",
+        {"form":form}
+    )
+
+    
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("issue-list")
+
+        return render(
+            request,
+            "accounts/login.html",
+            {"error": "Invalid username or password."}
+        )
+
+    return render(request, "accounts/login.html")
